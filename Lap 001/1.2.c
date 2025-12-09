470c0cf6b8d6a072cf11de0880dead20fe10bdba
chaiwat kong_in <nickolas18237@gmail.com>
2025-12-09 19:21:55 +0700

    Create 1.2.c

---
+++ Lap 001/1.2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void GetSet( int **receiver , int *countPtr ) {
+    int totalItems;
+    scanf( "%d" , &totalItems ) ;
+
+    int *tempBox = malloc( totalItems * sizeof(int ) ) ;
+
+    for( int slot = 0 ; slot < totalItems ; slot++ ) {
+        scanf( "%d" , &tempBox[slot] ) ;
+    }
+        
+    *receiver = tempBox ;
+    *countPtr = totalItems ;
+}
+
+int main() {
+    int *data, num ;
+    GetSet( &data , &num) ;
+
+    for( int i = 0 ; i < num; i++ ) {
+        printf( "%d " , data[i] ) ;
+    }
+
+    free( data ) ;
+    return 0 ;
+}
