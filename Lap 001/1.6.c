b7a8a2a9d785566145d5a7cf8253526ad394a67a
chaiwat kong_in <nickolas18237@gmail.com>
2025-12-14 23:19:19 +0700

    Update 1.6.c

--- Lap 001/1.6.c
+++ Lap 001/1.6.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *GetMatrix( int *rPtr , int *cPtr ) {
-    int r , c ;
-    scanf("%d %d", &r, &c ) ;
+int *GetMatrix(int *rPtr, int *cPtr) {
+    int r, c;
+    scanf("%d %d", &r, &c);
 
-    int total = r * c ;
-    int *box = malloc( total * sizeof( int ) ) ;
+    int total = r * c;
+    int *box = malloc(total * sizeof(int));
 
-    for ( int i = 0; i < total ; i++ ) {
-        scanf( "%d" , &box[i] ) ;
+    if (box == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
     }
 
-    *rPtr = r ;
-    *cPtr = c ;
+    for (int i = 0; i < total; i++) {
+        scanf("%d", &box[i]);
+    }
 
-    return box ;
+    *rPtr = r;
+    *cPtr = c;
+
+    return box;
+}
+
+void PrintMatrix(int *m, int r, int c) {
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ", m[i * c + j]);
+        }
+        printf("\n");
+    }
 }
 
 int main() {
-    int *data , m , n ;
-    data = GetMatrix( &m, &n ) ;
-    return 0 ;
+    int *data;
+    int m, n;
+
+    data = GetMatrix(&m, &n);
+
+    printf("Matrix (%d x %d):\n", m, n);
+    PrintMatrix(data, m, n);
+
+    free(data);
+    return 0;
 }
