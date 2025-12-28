802ab432ff1f25154b7c44ddebbdcbe313bc503b
chaiwat kong_in <nickolas18237@gmail.com>
2025-12-28 19:52:45 +0700

    Update 1.3.c

--- Basic/Lap 001/1.3.c
+++ Basic/Lap 001/1.3.c
@@ -3,24 +3,23 @@
 int main() {
     int score ;
 
-  
-    if ( scanf( "%d", &score ) != 1 ) {
+ 
+    if ( scanf( "%d" , &score) != 1 ) {
         return 1 ;
     }
 
 
-    if ( score >= 80 ) {
-        printf( "Grade A\n" ) ; 
-    } else if ( score >= 70 ) {
-        printf( "Grade B\n" ) ; 
-    } else if ( score >= 60 ) {
-        printf( "Grade C\n" ) ; 
+    if (score >= 80) {
+        printf( "Grade A" ) ;
+    } else if ( score >= 70 ){
+        printf( "Grade B" ) ;
+    } else if ( score >= 60) {
+        printf( "Grade C" ) ;
     } else if ( score >= 50 ) {
-        printf( "Grade D\n" ) ; 
-    } else {
-        printf( "Grade F\n" ) ;
+        printf( "Grade D" ) ;
+    } else if( score < 50 ) {
+        printf( "Grade F" ) ;
     }
 
-
     return 0 ;
-}
+} 
