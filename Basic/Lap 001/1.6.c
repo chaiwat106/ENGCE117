aace6d240b7396bad985e1a7d33002075e81f6cf
chaiwat kong_in <nickolas18237@gmail.com>
2025-12-28 21:47:09 +0700

    Create 1.6.c

---
+++ Basic/Lap 001/1.6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int number ;
+
+    do {
+     
+        printf( "Enter a number (1-10): ") ;
+        if ( scanf( "%d", &number) != 1) {
+           
+            while(getchar() != '\n') ; 
+            continue ;
+        }
+
+      
+        if ( number < 1 || number > 10) {
+            printf( "Error: Value must be 1-10.\n" ) ;
+        }
+
+    } while ( number < 1 || number > 10 ) ; 
+
+ 
+    printf( "Input accepted: %d\n", number) ;
+
+    return 0 ;
+}
