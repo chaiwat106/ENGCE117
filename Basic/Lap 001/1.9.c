2bdbbe4d6020a1b1768d5f7018fbdfe18e1b412e
chaiwat kong_in <nickolas18237@gmail.com>
2025-12-29 22:06:03 +0700

    Create 1.9.c

---
+++ Basic/Lap 001/1.9.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    int zoneCode ;
+    float weight_kg ;
+    float totalCost = 0.0 ;
+
+
+    if ( scanf( "%d %f" , &zoneCode , &weight_kg ) != 2 ) {
+        return 1 ;
+    }
+
+
+    switch ( zoneCode) {
+        case 1 :
+            if ( weight_kg <= 5 ) {
+                totalCost = 50.0 ;
+            } else {
+                totalCost = 80.0 ;
+            }
+            break ;
+
+        case 2 : 
+            if ( weight_kg <= 10) {
+                totalCost = 150.0 ;
+            } else {
+                totalCost = 250.0 ;
+            }
+            break;
+
+        case 3 : 
+            totalCost = 500.0 ; /
+            break ;
+
+        default: 
+            printf( "Invalid Zone Code\n" ) ;
+            return 0 ;
+    }
+
+  
+    if ( totalCost > 0.0 || zoneCode == 3 ) {
+        printf( "%.2f\n" , totalCost ) ;
+    }
+
+    return 0 ;
+}
