78b07ca09b552d7c4f6f49e3721b815bad9d7476
chaiwat kong_in <nickolas18237@gmail.com>
2026-03-08 15:15:38 +0700

    Create 11.2.c

---
+++ Lap 11/11.2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void mergesort( int t[] , int k ) ;
+void merge( int *u , int m , int *v , int n , int *t ) ;
+
+int main() {
+    int data[7] = { 4, 6, 1, 2, 5, 1, 8 } ;
+    int n = 7 ;
+    mergesort(data, n);
+    for ( int i = 0 ; i < n ; i++ ) printf( "%d " , data[i] ) ;
+    return 0 ;
+}
+
+void mergesort(int t[], int k) {
+    if (k > 1) {
+        int m = k / 2 ;
+        int n = k - m ;
+        int *u = (int*)malloc( m * sizeof(int) ) ;
+        int *v = (int*)malloc( n * sizeof(int) ) ;
+
+
+        for (int i = 0 ; i < m ; i++) u[i] = t[i] ;
+        for (int i = 0 ; i < n ; i++) v[i] = t[m + i] ;
+
+        mergesort( u , m ) ;
+        mergesort( v , n ) ;
+
+
+        merge( u , m , v , n , t ) ;
+
+        free(u) ;
+        free(v) ;
+    }
+}
+
+void merge(int *u, int m, int *v, int n, int *t) {
+    int i = 0 , j = 0 , k = 0 ;
+    
+    while (i < m && j < n) {
+        if (u[i] <= v[j]) {
+            t[k++] = u[i++] ;
+        } else {
+            t[k++] = v[j++] ;
+        }
+    }
+    
+ 
+    while (i < m) t[k++] = u[i++] ;
+    while (j < n) t[k++] = v[j++] ;
+}
